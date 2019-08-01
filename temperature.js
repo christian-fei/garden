1f5f9636999afba50dea3acf760d667232a8eda7
Christian Fei <crifei93@gmail.com>
2019-08-01 20:09:55 +0200

    promise main

--- temperature.js
+++ temperature.js
@@ -3,22 +3,24 @@
 const { promises: { read } } = require('node-dht-sensor')
 
 if (require.main === module) {
-  setImmediate(main, 11, 4)
+  main()
 } else {
   module.exports = main
 }
 
 async function main (type = 11, gpio = 4) {
-  console.log('temperature running')
+  return new Promise(async function processSensor () {
+    console.log('temperature running')
 
-  try {
-    const { temperature, humidity } = await read(type, gpio)
-    console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
-  } catch (err) {
-    console.error(err)
-  }
+    try {
+      const { temperature, humidity } = await read(type, gpio)
+      console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
+    } catch (err) {
+      console.error(err)
+    }
 
-  setTimeout(main, 10000, type, gpio)
+    setTimeout(processSensor, 10000)
+  })
 }
 
 process.on('unhandledRejection', (err) => console.error(err))
