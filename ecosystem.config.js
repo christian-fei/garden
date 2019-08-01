bc99f0af33af476276625177682c5bb1136ef66a
Christian Fei <crifei93@gmail.com>
2019-08-01 20:04:45 +0200

    pm2 update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,9 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     // args: 'one two',
     instances: 1,
-    autorestart: true,
+    // autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    // max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
