2526352d1300b6333325724edf72a97f4b49ec2a
Christian Fei <crifei93@gmail.com>
2019-08-01 19:56:06 +0200

    add node-dht-senor for temp & humidity

--- temperature.js
+++ temperature.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { promises: { read } } = require('node-dht-sensor')
+
 if (require.main === module) {
   main()
     .then(() => process.exit(0))
@@ -11,8 +13,11 @@ if (require.main === module) {
   module.exports = main
 }
 
-async function main () {
-  return new Promise(() => setInterval(() => {
-    console.log('temperature running')
-  }, 1000 * 60 * 5))
+async function main (type = 11, gpio = 4) {
+  console.log('temperature running')
+
+  const { temperature, humidity } = await read(type, gpio)
+  console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
+
+  setTimeout(main, 10000, type, gpio)
 }
