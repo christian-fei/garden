67b3862fd6a3180dadb7a039c4193cbd72a2479c
Christian Fei <crifei93@gmail.com>
2019-08-07 23:50:51 +0200

    pm2 conn

--- temperature-humidity.js
+++ temperature-humidity.js
@@ -4,37 +4,37 @@
 const pm2 = require('pm2')
 
 if (require.main === module) {
-  pm2.connect((err) => {
-    if (err) return console.error(err)
-
-    main()
-  })
+  main()
 } else {
   module.exports = main
 }
 
 async function main (type = 11, gpio = 4) {
-  console.log('temperature / humidity running')
+  pm2.connect((errConnect) => {
+    if (errConnect) return console.error(errConnect)
 
-  try {
-    // const { temperature, humidity } = await read(type, gpio)
-    // console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
-    console.log('running')
-    pm2.list((err, processes) => {
-      if (err) { return console.error(err) }
-      console.log('processes', processes)
-      for (const p of processes) {
-        pm2.sendDataToProcessId(p.pm_id, {
-          type: 'temperature',
-          temperature: 30,
-          humidity: 50
-        })
-      }
-    })
-    setTimeout(main, 10000, type, gpio)
-  } catch (err) {
-    console.log(err)
-  }
+    console.log('temperature / humidity running')
+
+    try {
+      // const { temperature, humidity } = await read(type, gpio)
+      // console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
+      console.log('running')
+      pm2.list((err, processes) => {
+        if (err) { return console.error(err) }
+        console.log('processes', processes)
+        for (const p of processes) {
+          pm2.sendDataToProcessId(p.pm_id, {
+            type: 'temperature',
+            temperature: 30,
+            humidity: 50
+          })
+        }
+      })
+      setTimeout(main, 10000, type, gpio)
+    } catch (err) {
+      console.log(err)
+    }
+  })
 }
 
 process.on('unhandledRejection', (err) => console.error(err))
