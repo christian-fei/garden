bbdd6932fb0549e31e9048f42ee4010d711b5e05
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-08 03:29:22 +0200

    adds telegram-bot to ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,10 @@ module.exports = {
   apps: [{
     name: 'temperature-humidity',
     script: 'temperature-humidity.js',
-    // args: 'run temperature-humidity',
     exec_mode: 'fork',
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
-    // autorestart: true,
     restart_delay: 3000,
     watch: false,
-    // max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
@@ -23,7 +19,19 @@ module.exports = {
     instances: 1,
     restart_delay: 3000,
     watch: false,
-    // max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }, {
+    name: 'telegram-bot',
+    script: 'telegram-bot.js',
+    exec_mode: 'fork',
+    instances: 1,
+    restart_delay: 3000,
+    watch: false,
     env: {
       NODE_ENV: 'development'
     },
@@ -31,7 +39,6 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }],
-
   deploy: {
     production: {
       user: 'pi',
