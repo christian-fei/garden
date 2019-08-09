7370638a3d5997c56de5e8ccfc35cecefd767784
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-09 12:44:33 +0200

    fixes error

--- devices/dht11.js
+++ devices/dht11.js
@@ -13,11 +13,11 @@ setImmediate(async function main (model, gpio, { assign } = Object) {
   try {
     const { temperature, humidity } = await read(model, gpio)
     console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
-    if (previous.temperature === temperature && previous.humidity === humidity) return
+    // if (previous.temperature === temperature && previous.humidity === humidity) return
     // notify only if value changed
-    await broadcast(basename(__filename), { temperature, humidity })
-    assign({ previous, { temperature, humidity })
-    console.log('%s | ipc broadcast', new Date().toISOString())
+    // await broadcast(basename(__filename), { temperature, humidity })
+    // assign(previous, { temperature, humidity })
+    // console.log('%s | ipc broadcast', new Date().toISOString())
   } catch (err) {
     console.log('%s | ', new Date().toISOString(), err)
   } finally {
