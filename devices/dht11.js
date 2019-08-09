2aac2ff92f19ebf7fcc67709866edf89ce5572fa
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-09 12:58:01 +0200

    updates broadcast condition

--- devices/dht11.js
+++ devices/dht11.js
@@ -13,11 +13,11 @@ setImmediate(async function main (model, gpio, { assign } = Object) {
   try {
     const { temperature, humidity } = await read(model, gpio)
     console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
-    // if (previous.temperature === temperature && previous.humidity === humidity) return
-    // notify only if value changed
-    // await broadcast(basename(__filename), { temperature, humidity })
-    // assign(previous, { temperature, humidity })
-    // console.log('%s | ipc broadcast', new Date().toISOString())
+    if (previous.temperature !== temperature || previous.humidity !== humidity) {
+      await broadcast(basename(__filename), { temperature, humidity })
+      assign(previous, { temperature, humidity })
+      console.log('%s | ipc broadcast of new values', new Date().toISOString())
+    }
   } catch (err) {
     console.log('%s | ', new Date().toISOString(), err)
   } finally {
