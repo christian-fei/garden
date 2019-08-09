f7c431d8b2721e007676df5e3e439c46d89cc073
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-09 12:38:34 +0200

    updates ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  },{
+  // },{
   //   name: 'ds18b20',
   //   script: 'devices/ds18b20.js',
   //   exec_mode: 'fork',
@@ -26,36 +26,37 @@ module.exports = {
   //     NODE_ENV: 'production'
   //   }
   // }, {
-    name: 'logger',
-    script: 'app/logger.js',
-    exec_mode: 'fork',
-    instances: 1,
-    restart_delay: 3000,
-    watch: false,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  } , {
-    name: 'bot',
-    script: 'app/bot.js',
-    exec_mode: 'fork',
-    instances: 1,
-    restart_delay: 3000,
-    watch: false,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+  //   name: 'logger',
+  //   script: 'app/logger.js',
+  //   exec_mode: 'fork',
+  //   instances: 1,
+  //   restart_delay: 3000,
+  //   watch: false,
+  //   env: {
+  //     NODE_ENV: 'development'
+  //   },
+  //   env_production: {
+  //     NODE_ENV: 'production'
+  //   }
+  // }, {
+  //   name: 'bot',
+  //   script: 'app/bot.js',
+  //   exec_mode: 'fork',
+  //   instances: 1,
+  //   restart_delay: 3000,
+  //   watch: false,
+  //   env: {
+  //     NODE_ENV: 'development'
+  //   },
+  //   env_production: {
+  //     NODE_ENV: 'production'
+  //   }
   }],
   deploy: {
     production: {
       user: 'pi',
-      host: '192.168.1.110',
+      // host: '192.168.1.110',
+      hsot: '151.62.218.46',
       ref: 'origin/master',
       repo: 'git@github.com:christian-fei/garden.git',
       path: '/home/pi/garden',
