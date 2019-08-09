9a672f4a51ae95743db7781b42d7b856cb2c4ca3
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-09 14:10:24 +0200

    improves logging

--- devices/dht11.js
+++ devices/dht11.js
@@ -13,13 +13,12 @@ setImmediate(async function main (model, gpio, { assign } = Object) {
   try {
     const { temperature, humidity } = await read(model, gpio)
     console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
+    setTimeout(main, 2500, model, gpio)
     if (previous.temperature !== temperature || previous.humidity !== humidity) {
       await broadcast(basename(__filename), { temperature, humidity })
       assign(previous, { temperature, humidity })
     }
   } catch (err) {
     console.error(err)
-  } finally {
-    setTimeout(main, 2500, model, gpio)
   }
 }, 11, 4)
