22395a96360cdd625ed944e82b1da6c190c18bfb
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-09 14:19:08 +0200

    disables broadcast

--- devices/dht11.js
+++ devices/dht11.js
@@ -4,20 +4,16 @@ const { basename } = require('path')
 const { promises: { read } } = require('node-dht-sensor')
 const { broadcast } = require('../lib/ipc')
 
-let previous = {
-  temperature: undefined,
-  humidity: undefined
-}
-
+const previous = { temperature: undefined, humidity: undefined }
 setImmediate(async function main (model, gpio, { assign } = Object) {
   try {
     const { temperature, humidity } = await read(model, gpio)
     console.log('%s | GPIO=%d Temperature=%d°C Humidity=%d%', new Date().toISOString(), gpio, temperature, humidity)
     setTimeout(main, 2500, model, gpio)
-    if (previous.temperature !== temperature || previous.humidity !== humidity) {
-      await broadcast(basename(__filename), { temperature, humidity })
-      assign(previous, { temperature, humidity })
-    }
+    // if (previous.temperature !== temperature || previous.humidity !== humidity) {
+    //   await broadcast(basename(__filename), { temperature, humidity })
+    //   assign(previous, { temperature, humidity })
+    // }
   } catch (err) {
     console.error(err)
   }
