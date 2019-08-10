3bce909ec8affec8d1e5b1e87180718057757c7e
Christian Fei <crifei93@gmail.com>
2019-08-10 16:37:47 +0200

    fixx ch

--- devices/dht11.js
+++ devices/dht11.js
@@ -22,8 +22,8 @@ schedule.scheduleJob('*/1 * * * *', async function () {
     history.push({ temperature, humidity, date: new Date().toISOString() })
     temperatureMoistureHistory.write(history)
     if (history.length > 3) {
-      const temperatureChart = sparkly(history.map((d, i) => [d.temperature]))
-      const humidityChart = sparkly(history.map((d, i) => [d.humidity]))
+      const temperatureChart = sparkly(history.map((d, i) => d.temperature))
+      const humidityChart = sparkly(history.map((d, i) => d.humidity))
       process.stdout.write(`\n${temperatureChart}\n`)
       process.stdout.write(`\n${humidityChart}\n`)
       await broadcast('temperature-history', temperatureChart)
