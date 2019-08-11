64900b6827eec13af954dffae42875938d55c859
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-11 16:37:31 +0200

    adds wiring to engage pump

--- app/telegram.js
+++ app/telegram.js
@@ -99,7 +99,7 @@ bot.on('callback_query', async ({ id, data, message: { message_id, chat: { id: c
     try {
       bot.answerCallbackQuery(id, { text: 'Connecting to pump, might take a while!' })
       bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id })
-      await forceOn()
+      await forceOn({timeout: 30000})
       bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 30 seconds. Do you need to stop pump earlier than that?`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
     } catch (err) {
       console.error(err)
@@ -111,7 +111,7 @@ bot.on('callback_query', async ({ id, data, message: { message_id, chat: { id: c
     try {
       bot.answerCallbackQuery(id, { text: 'Connecting to pump, might take a while!' })
       bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id })
-      await forceOn()
+      await forceOn({timeout: 60000})
       bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 1 minute. Do you need to stop pump earlier than that?`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
     } catch (err) {
       console.error(err)
@@ -123,7 +123,7 @@ bot.on('callback_query', async ({ id, data, message: { message_id, chat: { id: c
     try {
       bot.answerCallbackQuery(id, { text: 'Connecting to pump, might take a while!' })
       bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id })
-      await forceOn()
+      await forceOn({timeout: await forceOn({timeout: 120000})})
       bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 2 minutes. Do you need to stop pump earlier than that?`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
     } catch (err) {
       console.error(err)
@@ -135,7 +135,7 @@ bot.on('callback_query', async ({ id, data, message: { message_id, chat: { id: c
     try {
       bot.answerCallbackQuery(id, { text: 'Connecting to pump, might take a while!' })
       bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id })
-      await forceOn()
+      await forceOn({timeout: 300000})
       bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 5 minutes. Do you need to stop pump earlier than that?`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
     } catch (err) {
       console.error(err)
