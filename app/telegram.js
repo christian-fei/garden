ff6ed9c787ed8dab763c665edc5042e18d25e8d6
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-08-11 17:45:25 +0200

    switching up logics

--- app/telegram.js
+++ app/telegram.js
@@ -97,14 +97,12 @@ bot.on('callback_query', async ({ id, data, message: { message_id, chat: { id: c
 
   if (data === 'pump_on_30s') {
     try {
-      const timeout = 30000
-
       bot.answerCallbackQuery(id, { text: 'Connecting to pump, might take a while!' })
       bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id })
-      const message = await bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 30 seconds.`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
-      const video = await forceOn({ timeout, framerate: 10 })
-      bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id, message_id: message.message_id })
-      bot.sendVideo(chat_id, video, {}, { contentType: 'video/mp4' })
+      const message = await bot.sendMessage(chat_id, `Pump has been successfuly turned on and will be switched off in 30 seconds unless you force it manually.`, { reply_markup: { inline_keyboard: [[{ text: 'Stop Pump', callback_data: 'pump_off' }], [{ text: 'Cancel', callback_data: 'cancel' }]] } })
+      await forceOn({ timeout: 30000, framerate: 10 })
+      bot.editMessageText('`Pump has been successfuly turned on and has been running 30 seconds. Control video is loading...`', { chat_id, message_id: message.message_id, reply_markup: { inline_keyboard: [] } })
+      bot.sendVideo(chat_id, await takeVideo({ timeout: 10000 }), {}, { contentType: 'video/mp4' })
     } catch (err) {
       console.error(err)
       bot.sendMessage(chat_id, 'Something went wrong, please try again later.')
