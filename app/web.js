dca4a07dd631940fd6ae41f822f64a5d68ab6d75
Christian Fei <crifei93@gmail.com>
2019-08-14 10:42:10 +0200

    add routes to web /ip /report /video

--- app/web.js
+++ app/web.js
@@ -7,7 +7,9 @@ console.log('BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD', BASIC_AUTH_USERNAME, BAS
 
 const express = require('express')
 const basicAuth = require('express-basic-auth')
-const { takePhoto } = require('../lib/camera')
+const { takePhoto, takeVideo } = require('../lib/camera')
+const { gatherIP } = require('../lib/ip')
+const { gatherReport } = require('../lib/report')
 
 const app = express()
 app.listen(3000)
@@ -19,11 +21,27 @@ app.use(basicAuth({
   realm: 'Imb4T3st4pp'
 }))
 
+app.get('/ip', async (req, res) => {
+  res.end(await gatherIP())
+})
+app.get('/report', async (req, res) => {
+  res.end(await gatherReport())
+})
 app.get('/photo', async (req, res) => {
   const buffer = await takePhoto()
 
   res.writeHead(200, {
     'Content-Type': 'image/jpeg',
+    'Content-Length': buffer.length
+  })
+
+  res.end(buffer)
+})
+app.get('/video', async (req, res) => {
+  const buffer = await takeVideo()
+
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4',
     'Content-Length': buffer.length
   })
 
