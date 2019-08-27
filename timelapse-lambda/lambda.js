216f6fc78da2e0cd3e7d5cbe1a34a3b7b83c5b05
Christian Fei <crifei93@gmail.com>
2019-08-27 11:39:11 +0200

    save timelapse to s3

--- timelapse-lambda/lambda.js
+++ timelapse-lambda/lambda.js
@@ -4,55 +4,62 @@ const { promisify } = require('util')
 const fs = require('fs')
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3()
+const listObjects = promisify(S3.listObjects).bind(S3)
 const getObject = promisify(S3.getObject).bind(S3)
+const putObject = promisify(S3.putObject).bind(S3)
 
-// const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 const ffmpeg = require('fluent-ffmpeg')
+// const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 // ffmpeg.setFfmpegPath(ffmpegPath)
 
 exports.handler = function (event, context, cb) {
-  S3.listObjects({
+  const instance = ffmpeg()
+  const outFilePath = `/tmp/out.mp4`
+
+  listObjects({
     Bucket: event.Bucket
-  }, function (err, data) {
-    if (err) {
-      console.error(err)
-      return cb(err)
-    }
+  })
+    .then(data => {
+      const amount = Number.isFinite(event.Amount) ? event.Amount : 10
+      const files = data.Contents.filter((d, i) => i > (data.Contents.length - 1 - amount)).map(d => d.Key)
 
-    const amount = Number.isFinite(event.Amount) ? event.Amount : 10
-    const files = data.Contents.filter((d, i) => i > data.Contents.length - 1 - amount).map(d => d.Key)
+      return Promise.all(files.map(file => {
+        var params = {
+          Bucket: event.Bucket,
+          Key: file
+        }
+        return getObject(params)
+      }))
+    })
+    .then(results => {
+      results.forEach((r, i) => {
+        const tmpFilePath = `/tmp/${i}.jpg`
+        fs.writeFileSync(tmpFilePath, r.Body)
+        instance.addInput(tmpFilePath)
+      })
 
-    const instance = ffmpeg()
-    // console.log('ffmpegPath', ffmpegPath)
-    // cb(null, ffmpegPath)
-    // cb(null, JSON.stringify(instance))
-
-    const outFilename = `/tmp/out.mp4`
-
-    Promise.all(files.map(file => {
-      var params = {
-        Bucket: event.Bucket,
-        Key: file
-      }
-      return getObject(params)
-    }))
-      .then(results => {
-        // cb(null, results.map(r => JSON.stringify(r)))
-        results.forEach((r, i) => {
-          const tmpFilePath = `/tmp/${i}.jpg`
-          console.log(r)
-          fs.writeFileSync(tmpFilePath, r.Body)
-          instance.addInput(tmpFilePath)
-        })
+      return new Promise((resolve, reject) => {
         instance
           .on('end', function () {
             console.log('Processing finished !')
-            cb(null, { files: results.length, cwd: process.cwd(), out: fs.readFileSync(outFilename) })
+            resolve()
           })
-          .save(outFilename, { end: true })
+          .save(outFilePath, { end: true })
       })
-      .catch(err => {
-        cb(err)
+    })
+    .then(() => {
+      var params = {
+        // ACL: 'authenticated-read',
+        Body: fs.readFileSync(outFilePath),
+        Bucket: event.Bucket,
+        Key: 'timelapse.mp4'
+      }
+      S3.putObject(params, function (err, data) {
+        cb(err, data)
+        // cb(null, { success: true, file: outFilePath })
       })
-  })
+    })
+    .catch(err => {
+      cb(err)
+    })
 }
